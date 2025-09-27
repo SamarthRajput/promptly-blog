5113215f916ad55ff2822aa3e5f944c296336e66
Samarth Rajput <ssamarth224@gmail.com>
2025-09-27 13:25:08 +0530

    generating title using ai and changed the ai model

--- utils/generateGeminiResponse.ts
+++ utils/generateGeminiResponse.ts
@@ -5,7 +5,7 @@ export const generateGeminiResponse = async (prompt: string) => {
         throw new Error("GEMINI_API_KEY is not defined");
     }
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
     const result = await model.generateContent(prompt);
     return result.response.text();
